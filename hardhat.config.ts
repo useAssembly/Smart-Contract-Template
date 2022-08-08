df1e6fe473380da4f5331d33d786d02e5e904a9f
John <jonathantjh@hotmail.com>
2022-08-08 17:16:32 +0800

    first commit

---
+++ hardhat.config.ts
@@ -0,0 +1,35 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+
+const config: HardhatUserConfig = {
+  solidity: "0.8.9",
+};
+
+require("@nomicfoundation/hardhat-toolbox");
+
+// Go to https://www.alchemyapi.io, sign up, create
+// a new App in its dashboard, and replace "KEY" with its key
+const ALCHEMY_API_KEY = "w3vGHL0-yqI_cloArsYEbSB9kIv9ZFVv";
+
+// Replace this private key with your Goerli account private key
+// To export your private key from Metamask, open Metamask and
+// go to Account Details > Export Private Key
+// Beware: NEVER put real Ether into testing accounts
+const GOERLI_PRIVATE_KEY = "63814231b2125a107c0b27c7dc902ffef351178aece281b3f39463864ce8dbf6";
+
+module.exports = {
+  solidity: "0.8.9",
+  networks: {
+    goerli: {
+      url: `https://eth-goerli.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
+      accounts: [GOERLI_PRIVATE_KEY]
+    }
+  },
+  etherscan: {
+    apiKey: "9C7VC821GQ5T26RZQTEUXRPYR2MR35DRCI",
+  },
+};
+
+
+
+export default config;
---
+++ scripts/deploy.ts
@@ -0,0 +1,18 @@
+import { ethers } from "hardhat";
+
+async function main() {
+
+  const Lock = await ethers.getContractFactory("MudCats");
+  const lock = await Lock.deploy();
+
+  await lock.deployed();
+
+  console.log("Contract deployed to:", lock.address);
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
