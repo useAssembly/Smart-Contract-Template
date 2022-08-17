fc684789a4c0230d00d124c06a2f1569f6acf577
jeraldgan <jeraldgantc@gmail.com>
2022-08-17 19:17:15 +0800

    WIP staking test

---
+++ test/AFStaking/AFStaking.ts
@@ -0,0 +1,59 @@
+import { loadFixture } from "@nomicfoundation/hardhat-network-helpers";
+import { expect } from "chai";
+import { ethers } from "hardhat";
+const {
+  constants, // Common constants, like the zero address and largest integers
+} = require("@openzeppelin/test-helpers");
+
+describe("Staking", function () {
+  // We define a fixture to reuse the same setup in every test.
+  // We use loadFixture to run this setup once, snapshot that state,
+  // and reset Hardhat Network to that snapshopt in every test.
+  async function deployAirfoilToken() {
+    const AirfoilToken = await ethers.getContractFactory("AirfoilToken");
+    const airfoilToken = await AirfoilToken.deploy();
+
+    return { airfoilToken };
+  }
+
+  const deployerAddress = "0x916081C20245D239125e3043A6BA519935610edF";
+  const toAddress = "0x3dB716519Db5CfCaD3d591606e0E2c7D944668eE";
+
+  describe("Deployment", function () {
+    it("Should deploy contract successfully", async function () {
+      const { airfoilToken } = await loadFixture(deployAirfoilToken);
+
+      expect(await airfoilToken.balanceOf(deployerAddress)).to.equal(0);
+    });
+  });
+
+  describe("Minting", function () {
+    it("Should assign initial balance via minting", async function () {
+      const { airfoilToken } = await loadFixture(deployAirfoilToken);
+
+      await airfoilToken.mint(deployerAddress, 1000);
+
+      expect(await airfoilToken.balanceOf(deployerAddress)).to.equal(1000);
+    });
+
+    it("Minting should emit transfer event", async function () {
+      const { airfoilToken } = await loadFixture(deployAirfoilToken);
+
+      await expect(airfoilToken.mint(deployerAddress, 1000))
+        .to.emit(airfoilToken, "Transfer")
+        .withArgs(constants.ZERO_ADDRESS, deployerAddress, 1000);
+    });
+  });
+
+  describe("Transfer", function () {
+    it("Should not be able to transfer amount more than balance", async function () {
+      const { airfoilToken } = await loadFixture(deployAirfoilToken);
+
+      await airfoilToken.mint(deployerAddress, 1000);
+
+      await expect(airfoilToken.transfer(toAddress, 1007)).to.be.revertedWith(
+        "ERC20: transfer amount exceeds balance"
+      );
+    });
+  });
+});
